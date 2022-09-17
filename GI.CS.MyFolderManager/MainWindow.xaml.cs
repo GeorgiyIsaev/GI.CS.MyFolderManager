4f658a5d7b3a9a225588d554dfa8abc3c63fd0c0
Георгий ГераМера <germesutan@gmail.com>
2022-09-17 22:22:43 +0300

    Список папок в папке

--- GI.CS.MyFolderManager/MainWindow.xaml.cs
+++ GI.CS.MyFolderManager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,21 @@ namespace GI.CS.MyFolderManager
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += Window_ManagementProfile_Loaded;
         }
+
+        private void Window_ManagementProfile_Loaded(object sender, RoutedEventArgs e)
+        {            
+        }
+
+        ObservableCollection<TablesTest> tablesTest = new ObservableCollection<TablesTest>();
+        public class TablesTest
+        {
+            public String NameCatalog { get; set; }
+            public int CountFile { get; set; }
+   
+        }
+
 
 
         private void Button_EnterCatalog_Click(object sender, RoutedEventArgs e)
@@ -31,10 +46,22 @@ namespace GI.CS.MyFolderManager
             if (dialog.ShowDialog(this).GetValueOrDefault())
             {
                 Lebel_EnterCatalog.Content = dialog.SelectedPath;
-            }           
+                ListCatalog();
+            }
         }
 
-
+        private void ListCatalog(){
+        
+            DataGrid_Catalog.ItemsSource = null; //сборс
+            tablesTest = new ObservableCollection<TablesTest>();
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Lebel_EnterCatalog.Content.ToString());
+            foreach (var item in dir.GetDirectories())
+            {
+                tablesTest.Add(new TablesTest { NameCatalog = item.Name, /*Id = testP.Id, GroupName = testP.Group, TestName = testP.Name, Count = testP.Quests.Count()*/});
+            }    
+            DataGrid_Catalog.ItemsSource = tablesTest;
+            DataGrid_Catalog.Items.Refresh();
+        }
 
     }
 }
