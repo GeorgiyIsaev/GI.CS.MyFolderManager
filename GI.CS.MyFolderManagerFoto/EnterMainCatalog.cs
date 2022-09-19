39056f0990b9bf3850a62c9b66c9fb9fbd4f4aee
Георгий ГераМера <germesutan@gmail.com>
2022-09-19 14:54:28 +0300

    Получение каталога

---
+++ GI.CS.MyFolderManagerFoto/EnterMainCatalog.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GI.CS.MyFolderManagerFoto
+{
+    public partial class MainWindow
+    {
+        /*Открывает папку и отобржает содержимое*/
+        private void Button_EnterCatalog_Click(object sender, RoutedEventArgs e)
+        {
+            /*Выбор каталога*/
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                Lebel_EnterCatalog.Content = dialog.SelectedPath;
+                ListCatalog();
+               // ListCatalogMatch();
+            }
+        }
+
+        ObservableCollection<InfoCatalog> infoCatalog = new ObservableCollection<InfoCatalog>();
+
+        private void ListCatalog()
+        {
+
+            DataGrid_Catalog.ItemsSource = null;
+            infoCatalog = new ObservableCollection<InfoCatalog>();
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Lebel_EnterCatalog.Content.ToString());
+            foreach (var item in dir.GetDirectories())
+            {
+                infoCatalog.Add(new InfoCatalog { NameCatalog = item.Name, CountFile = ListCatalogCountFile(item.Name) });
+            }
+            DataGrid_Catalog.ItemsSource = infoCatalog;
+            DataGrid_Catalog.Items.Refresh();
+
+
+        }
+        private int ListCatalogCountFile(string NameCatalog)
+        {
+            int countFile = new System.IO.DirectoryInfo(Lebel_EnterCatalog.Content + "\\" + NameCatalog)
+                .GetFiles("*.*", System.IO.SearchOption.AllDirectories).Length;
+            return countFile;
+        }
+
+        private void DataGrid_Catalog_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        
+
+        private void Button_V_Click(object sender, RoutedEventArgs e)
+        {  /*Достает колонку с повторами*/
+
+            if (Column_0.Width.Value > 0)
+            {
+                Column_0.Width = new GridLength(0);
+            }
+            else
+            {
+                Column_0.Width = new GridLength(300);
+            }
+        }
+    }
+}
---
+++ GI.CS.MyFolderManagerFoto/InfoCatalog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GI.CS.MyFolderManagerFoto
+{
+    public class InfoCatalog
+    {
+        public String NameCatalog { get; set; }
+        public int CountFile { get; set; }
+
+
+
+        public List<String> SplitName()
+        {
+            List<String> nameItem = new List<String>(NameCatalog.Split(' ', '-'));
+            nameItem.RemoveAll(EndsWithSaurus); //Удалить пустые строчки
+            return nameItem;
+        }
+
+        private static bool EndsWithSaurus(String s)
+        {
+            return s.Length < 1;
+        }
+    }
+}
