bb155ad020419a2890dbe72269d95551ea20e771
Георгий ГераМера <germesutan@gmail.com>
2022-09-20 20:08:18 +0300

    Вход в тег

--- GI.CS.MyFolderManagerFoto/EnterTags.cs
+++ GI.CS.MyFolderManagerFoto/EnterTags.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -17,7 +18,9 @@ namespace GI.CS.MyFolderManagerFoto
 
             foreach (InfoCatalog val in infoCatalogs)
             {
-                if(val.tags.Count == 0)
+                AddDictionary("All", val.NameCatalog);
+
+                if (val.tags.Count == 0)
                 {
                     AddDictionary("!Нет тега!", val.NameCatalog);
                     continue;
@@ -44,11 +47,11 @@ namespace GI.CS.MyFolderManagerFoto
             foreach(var val in TagAndCatalogs)
             {
                 Button btn = new Button();
-                btn.Content = " " + val.Key + " (" + val.Value.Count + ")";
-                btn.Click += MyBtn_Click;
+                btn.Content = " " + val.Key + " (" + val.Value.Count + ")"; 
+                btn.Tag = val;
 
-
-
+                var d = btn.Tag;
+                btn.Click += Button_EnterTag;
 
 
                 TextBox_Tags.Children.Add(btn);    
@@ -56,13 +59,27 @@ namespace GI.CS.MyFolderManagerFoto
                 //добавить действие с заполнением таблицы
             }
         } 
-        private void MyBtn_Click(Object sender, EventArgs e)
+        private void Button_EnterTag(Object sender, EventArgs e)
         {
-
-            MessageBox.Show("");
-            //MyMethod((sender as Button).Tag);
+            var val = ((Button)sender);
+            KeyValuePair<string, List<string>> para = ((KeyValuePair<string, List<string>>)val.Tag);
+            EnterTag(para.Value);     
         }
 
+
+        private void EnterTag(List<string> catalogs)
+        {
+            /*Показать папки имеющие повторы*/
+            DataGrid_Catalog.ItemsSource = null;
+            infoCatalogSearch = new ObservableCollection<InfoCatalog>();
+            foreach (var item in catalogs)
+            {
+                infoCatalogSearch.Add(new InfoCatalog(item, Lebel_EnterCatalog.Content.ToString()));     
+            }
+
+            DataGrid_Catalog.ItemsSource = infoCatalogSearch;
+            DataGrid_Catalog.Items.Refresh();
+        }
 
     }
 }
